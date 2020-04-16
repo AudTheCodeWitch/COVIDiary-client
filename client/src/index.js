08b31aba3134221105312ef1533e7101c1c62b56
Audrea Cook <audreacwork@gmail.com>
2020-04-16 14:39:29 -0600

    Install and import dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './custom.scss';
+
+// set up store
 
 ReactDOM.render(
   <React.StrictMode>
