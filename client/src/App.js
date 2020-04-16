9bb88e5742a24404ef4d6a9be748d67d704719bf
Audrea Cook <audreacwork@gmail.com>
2020-04-16 14:40:09 -0600

    Test connection to api

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+
+  // test fetch
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/users/1/entries')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }
+
   render() {
     return (
       <div className="App">
